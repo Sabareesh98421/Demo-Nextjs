9a0493d4057b40635dd219b5ef7261748bd64685
Sabareesh98421 <pushpakumar5409@gmail.com>
2025-11-05 15:37:15 +0530

    Refactor:Replace the useState and useEffect Hooks with RTXQuery and Added some loader and stuffs..

--- app/vote/result/page.tsx
+++ app/vote/result/page.tsx
@@ -1,25 +1,40 @@
 "use client"
 // ./result/page.tsx
-
 import Link from "next/link";
-import { useEffect, useState } from "react";
+
+import {useGetVoteResultsQuery} from "@/features/RTK/Query/vote/Result/PoolingResultApi";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import CircularProgress from "@mui/material/CircularProgress";
+import LinearProgress from "@mui/material/LinearProgress";
+
 type ResponseData = Record<string, number>;
 export default function Result() {
-    const [results, setResults] = useState<ResponseData | null>(null);
-    
-    // Derived State: True if we haven't successfully fetched anything yet
-    const isLoading = results === null;
-    useEffect(() => {
-        getResults(setResults)
-    }, [])
+
+    const {data:results,error,isLoading} = useGetVoteResultsQuery();
+    if(error){
+        return (
+            <p className="text-red-500 text-center mt-10">
+                Failed to load results. Please try again later.
+            </p>
+        );
+    }
     return (
         <div className="p-8 max-w-lg mx-auto bg-white shadow-xl rounded-lg text-black">
+
             <h1 className="text-3xl font-bold mb-6 text-center border-b pb-2">🗳️ Poll Results</h1>
 
             {/* 1. LOADING STATE */}
             {isLoading && (
-                <p className="text-lg text-gray-600">Loading current vote counts...</p>
+                <Box component="div" className="text-lg text-gray-600 flex justify-evenly items-center gap-5">
+                    <Typography>
+                    Loading current vote counts...
+                    </Typography>
+                    <CircularProgress size={25}/>
+                    {/*<LinearProgress/>*/}
+                </Box>
             )}
+            {/*<Loading></Loading>*/}
 
             {/* 3. RESULTS DISPLAY: If results is NOT null and contains data */}
             {results && Object.keys(results).length > 0 && (
@@ -46,7 +61,7 @@ export default function Result() {
 
             <div className="mt-8 text-center">
                 <Link href="/" className="text-blue-500 hover:text-blue-700 underline">
-                    🔙 Go back to the main page
+                    Go back to the main page
                 </Link>
             </div>
         </div>

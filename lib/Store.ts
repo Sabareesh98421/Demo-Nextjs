eaae8596ece4dc214a16266507033cffe9a491a2
Sabareesh98421 <pushpakumar5409@gmail.com>
2025-11-05 15:38:34 +0530

    Config:set the 'RTk' Query Reducer in the store

--- lib/Store.ts
+++ lib/Store.ts
@@ -1,15 +1,19 @@
 // store.ts
 import {configureStore} from "@reduxjs/toolkit";
 import countSlicer from "@/features/CounterSlice/counterSlice";
+import {pollingResultsAPI} from "@/features/RTK/Query/vote/Result/PoolingResultApi";
 
 
 export function demoStore(){
     return configureStore({
         reducer:
             {
+                [pollingResultsAPI.reducerPath]:pollingResultsAPI.reducer,
                 counter:countSlicer,
                 // userData:userSlicer
-            }
+
+            },
+        middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(pollingResultsAPI.middleware)
     })
 }
 

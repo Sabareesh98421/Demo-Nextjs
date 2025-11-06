43fef08e79010a09f6a70d6abd04d0f9173c4781
Sabareesh98421 <pushpakumar5409@gmail.com>
2025-11-06 16:04:00 +0530

    Feat:NavBarComponent

---
+++ components/Nav/Nav.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+import {ThemeToggle} from "@/components/ThemeToggle/themeToggle";
+
+
+export function Nav(){
+    return(
+        <>
+            <ThemeToggle/>
+        </>
+    )
+}

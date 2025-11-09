e58d2e13f5060880d587a368c407fa28f55a7a3d
Sabareesh98421 <pushpakumar5409@gmail.com>
2025-11-09 17:59:27 +0530

    Feat:Create a login Mutation API slice

---
+++ features/RTK/Query/loginAPI/Loginapi.ts
@@ -0,0 +1,16 @@
+import {AppAPI} from "@/features/RTK/CreateAPI/DefineAppApi";
+import {LoginFormData, LoginResponse} from "@/sharedUtils/CustomTypes";
+import {HTTP_Method} from "@/serverUtils/Enums/HTTP_Enum";
+
+export const loginAPISlice = AppAPI.injectEndpoints({
+    endpoints:(build)=>({
+        loginUser:build.mutation<LoginResponse,LoginFormData>({
+            query:(userData:LoginFormData)=>({
+                url:"LogIn",
+                method:HTTP_Method.POST,
+                body:userData
+            })
+        })
+    })
+});
+export const   {useLoginUserMutation} = loginAPISlice;

6623483f194fffadc2ab0853c647c25b32dcfbdf
Sabareesh98421 <pushpakumar5409@gmail.com>
2025-11-10 19:36:44 +0530

    Feat:Added Middle ware for Auth and other stuffs

---
+++ proxy.ts
@@ -0,0 +1,18 @@
+import {NextRequest} from "next/server";
+import {jwtParserMiddleWare} from "@/serverUtils/middleWares/jwtParser";
+import {next, redirect} from "@/serverUtils/ServerResponse";
+
+export function proxy(req:NextRequest){
+    const pathName=req.nextUrl.pathname;
+
+    const token = req.cookies.get("token")?.value;
+    if(!token)
+    {
+
+        return redirect(new URL("/signIn",req.url))
+    }
+    return jwtParserMiddleWare(token,pathName)
+}
+export const config={
+    matcher:[ '/((?!api/LogIn|api/signup|signIn|signUp|ForgotPassword|_next|favicon\\.ico|images|fonts).*)',]
+}

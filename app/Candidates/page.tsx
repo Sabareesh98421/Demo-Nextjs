2967c3b8c9b8651b5064e990e8c894fffad8be29
Sabareesh98421 <pushpakumar5409@gmail.com>
2025-11-10 11:06:05 +0530

    Create: a Component that holds the entier candidates lists and details

---
+++ app/Candidates/page.tsx
@@ -0,0 +1,38 @@
+"use client"
+import Typography from  "@mui/material/Typography";
+import Grid from "@mui/material/Grid"
+import {useState} from "react";
+import {candidates} from "@/data/candidateList";
+import CandidateList from "@/components/candidatesList";
+export default function Candidates(){
+    const [isDetailedOpened,setDetailedOpen] = useState<number|null>(null);
+
+
+    return (
+            <Grid className="overflow-auto overflow-x-hidden h-full "  container
+                  direction="column">
+                    <Typography component="h1"  textAlign="center" m={2} >
+                        Candidate Lists
+                    </Typography>
+                <Grid container  spacing={3}>
+
+                    {
+                        candidates.map((candidate,i)=>(
+                            <Grid
+                                item
+                                key = {candidate.name}
+                                xs={12}
+                                sm={isDetailedOpened===i?12:6}
+                                sx={{ transition: "all 0.4s ease" }}
+                            >
+                                <CandidateList {...candidate} expanded={isDetailedOpened===i} onClick={()=>(setDetailedOpen(isDetailedOpened===i?null:i))}></CandidateList>
+                            </Grid>
+                            )
+
+                        )
+                    }
+
+                </Grid>
+            </Grid>
+    )
+}

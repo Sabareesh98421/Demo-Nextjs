3617f9f28eeacd7abeb1ca4c08811077698ab081
Sabareesh98421 <pushpakumar5409@gmail.com>
2025-11-10 19:38:54 +0530

    Feat:add more utils functions

--- serverUtils/ServerResponse.ts
+++ serverUtils/ServerResponse.ts
@@ -1,11 +1,19 @@
 import {NextResponse} from "next/server";
 import {ResponseGeneratorParam} from "@/sharedUtils/CustomTypes";
 
-export function serverResponse<T=unknown>({status = 200, message = "ok", data= null}:ResponseGeneratorParam<T>){
-    const res = NextResponse;
-    if(data){
-        return res.json({message,data},{status})
-    }
-    return res.json({message},{status})
 
+export function serverResponse<T=unknown>({status = 200, message = "ok", data= null}:ResponseGeneratorParam<T>){
+    if(data){
+        return NextResponse.json({message,data},{status})
+    }
+    return NextResponse.json({message},{status})
+
+}
+
+export function redirect(path:URL|string)
+{
+       return  NextResponse.redirect(path);
+}
+export function next(){
+    return NextResponse.next()
 }

0f3f583ebd094cb90d44ef8b0f8e1cace3319762
Sabareesh98421 <pushpakumar5409@gmail.com>
2025-11-10 19:39:27 +0530

    Feat:Set Cookie after login successfully

--- app/api/LogIn/route.ts
+++ app/api/LogIn/route.ts
@@ -1,6 +1,9 @@
 import {FilesHandling} from "@/serverUtils/fileHandling";
 import {LoginFormData, UserData} from "@/sharedUtils/CustomTypes";
 import {serverResponse} from "@/serverUtils/ServerResponse";
+import {cookies} from "next/headers";
+import {createJWT} from "@/serverUtils/JWT/jwt";
+
 type isUserValid={
     isPasswordMatched:boolean,
     isEmailMatched:boolean
@@ -9,19 +12,35 @@ type isUserValid={
 const fs = new FilesHandling("users.json");
 export async function POST(req:Request){
     const {email,password} = await req.json() as LoginFormData;
+    const loweredEmail=email.toLowerCase();
     await fs.ensureDataFile([]);
-    const {isPasswordMatched,isEmailMatched} = await isUserValid(email,password)
+    const {isPasswordMatched,isEmailMatched} = await isUserValid(loweredEmail,password)
 
-    if(isPasswordMatched && isEmailMatched){
-        console.log("User",email,"is logging in")
-        return serverResponse({message:"User Exists Logging in"});
+    if(!(isPasswordMatched && isEmailMatched)){
+        return serverResponse({status:403,message:"User doesn't not exist"})
     }
     if( isEmailMatched && !isPasswordMatched){
         return serverResponse({status:401,message:"Password incorrect"})
     }
-    return serverResponse({status:403,message:"User doesn't not exist"})
+    console.log("User",loweredEmail,"is logging in");
+    const token = createJWT(loweredEmail);
+    await setCookies(loweredEmail,token);
+
+    return serverResponse({message:"User Exists Logging in"});
 }
 
+async function setCookies(email:string,token:string){
+    const cookiesStore = await  cookies();
+    cookiesStore.set({
+        name:"token",
+        value:token,
+        httpOnly:true,
+        secure:process.env.NODE_ENV=== "production",
+        path:"/",
+        maxAge:60*60*2,
+        sameSite:"lax"
+    })
+}
 async function isUserValid (email:string,password:string):Promise<isUserValid>{
     const  isLegitUser:UserData[]= await fs.readDataJson<UserData[]>();
 

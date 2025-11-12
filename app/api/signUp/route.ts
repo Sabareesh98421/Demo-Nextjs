2213111c365931b75346eaf07d64d07d93d75bf3
Sabareesh98421 <pushpakumar5409@gmail.com>
2025-11-12 18:07:41 +0530

    Refactor:create a admin user automatically when the very first Registration occurs

--- app/api/signUp/route.ts
+++ app/api/signUp/route.ts
@@ -1,5 +1,5 @@
 import {FilesHandling} from "@/serverUtils/fileHandling";
-import {SignUpFormData, UserData} from "@/sharedUtils/CustomTypes";
+import {Role, SignUpFormData, UserData} from "@/sharedUtils/CustomTypes";
 import {NextResponse} from "next/server";
 
 const fs = new FilesHandling("users.json")
@@ -15,14 +15,30 @@ function hasMissingFields(userData:SignUpFormData){
 function isExistingUserFound(existingData:UserData[],email:string){
     return existingData.some((eachUser)=>eachUser.email === email)
 }
+function adminCreds():UserData{
+    const [adminEmail,adminPassword]=[process.env.ADMIN_EMAIL,process.env.ADMIN_PASS];
+    if(!(adminEmail && adminPassword)){
+        throw  new ReferenceError("the env file needs to contains the admin credentials but found none, so please add those in the .env file ")
+    }
 
+    return {
+            email:adminEmail,
+            password:adminPassword,
+            role:Role.Admin
+    }
+
+}
 export async function POST(req:Request){
     const res = NextResponse;
     try{
             await fs.ensureDataFile<UserData[]>([]);
             const userdata:SignUpFormData = await req.json();
             const {email,password,confirmPassword} = userdata;
-            const existingData=await fs.readDataJson<UserData[]>()
+            const  existingData:UserData[]=await fs.readDataJson<UserData[]>();
+            if(existingData.length===0){
+                const admin:UserData = adminCreds();
+                existingData.unshift(admin);
+            }
             const loweredEmail:string = email.toLowerCase();
             if( isExistingUserFound(existingData,loweredEmail)){
                 return res.json({message:"User already exists"},{status:409});
@@ -39,7 +55,8 @@ export async function POST(req:Request){
 
             const finalData={
                 email:loweredEmail,
-                password
+                password,
+                role:Role.User
             }
             existingData.push(finalData)
             await fs.writeDataJson<UserData[]>(existingData);

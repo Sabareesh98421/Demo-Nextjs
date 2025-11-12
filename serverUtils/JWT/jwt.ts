6f5b1a1e53192a508081a5ca5ae7b9545507c976
Sabareesh98421 <pushpakumar5409@gmail.com>
2025-11-12 18:05:34 +0530

    Refactor:update the token payload with userRole

--- serverUtils/JWT/jwt.ts
+++ serverUtils/JWT/jwt.ts
@@ -1,11 +1,11 @@
 import jwt, {Secret} from "jsonwebtoken";
 import {serverResponse} from "@/serverUtils/ServerResponse";
+import {Role} from "@/sharedUtils/CustomTypes";
 
 const secretKey:Secret = process.env.SECRET_KEY!;
 
-export function createJWT(email:string){
-    console.log("secretKey : ",secretKey)
-    return jwt.sign({email}, secretKey, {expiresIn: "2h"});
+export function createJWT(email:string,role:Role){
+    return jwt.sign({email,role}, secretKey, {expiresIn: "2h"});
 }
 export function parseJWT(token:string){
     try {
@@ -13,7 +13,7 @@ export function parseJWT(token:string){
         return serverResponse({message:"Token Verified successfully",status:200})
 
     }
-    catch(err:JsonWebTokenError|TokenExpiredError){
+    catch(err:unknown){
         if(err instanceof  jwt.JsonWebTokenError){
           return   serverResponse({message:"Token corrupted ",status:401})
         }

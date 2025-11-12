01a45ad31dd8915947ecd1f0c06dc5ebb9625a5d
Sabareesh98421 <pushpakumar5409@gmail.com>
2025-11-12 18:04:55 +0530

    Update:update the Token payload with role in the create Token Function

--- app/api/LogIn/route.ts
+++ app/api/LogIn/route.ts
@@ -1,12 +1,13 @@
 import {FilesHandling} from "@/serverUtils/fileHandling";
-import {LoginFormData, UserData} from "@/sharedUtils/CustomTypes";
+import {LoginFormData, Role, UserData} from "@/sharedUtils/CustomTypes";
 import {serverResponse} from "@/serverUtils/ServerResponse";
 import {cookies} from "next/headers";
 import {createJWT} from "@/serverUtils/JWT/jwt";
 
-type isUserValid={
+type isUserValid<T>={
     isPasswordMatched:boolean,
-    isEmailMatched:boolean
+    isEmailMatched:boolean,
+    data?:T|null
 }
 
 const fs = new FilesHandling("users.json");
@@ -14,7 +15,7 @@ export async function POST(req:Request){
     const {email,password} = await req.json() as LoginFormData;
     const loweredEmail=email.toLowerCase();
     await fs.ensureDataFile([]);
-    const {isPasswordMatched,isEmailMatched} = await isUserValid(loweredEmail,password)
+    const {isPasswordMatched,isEmailMatched,data} = await isUserValid(loweredEmail,password)
 
     if(!(isPasswordMatched && isEmailMatched)){
         return serverResponse({status:403,message:"User doesn't not exist"})
@@ -23,13 +24,19 @@ export async function POST(req:Request){
         return serverResponse({status:401,message:"Password incorrect"})
     }
     console.log("User",loweredEmail,"is logging in");
-    const token = createJWT(loweredEmail);
-    await setCookies(loweredEmail,token);
+    if(!data){
+        return serverResponse({status:500,message:"Unexpected error"});
+    }
+    const token = createJWT(loweredEmail,data.role);
+    await setCookies(token);
+    if(data.role==Role.Admin ){
+        return serverResponse({message:"Welcome Admin "});
+    }
 
     return serverResponse({message:"User Exists Logging in"});
 }
 
-async function setCookies(email:string,token:string){
+async function setCookies(token:string){
     const cookiesStore = await  cookies();
     cookiesStore.set({
         name:"token",
@@ -41,17 +48,17 @@ async function setCookies(email:string,token:string){
         sameSite:"lax"
     })
 }
-async function isUserValid (email:string,password:string):Promise<isUserValid>{
+async function isUserValid (email:string,password:string):Promise<isUserValid<UserData>>{
     const  isLegitUser:UserData[]= await fs.readDataJson<UserData[]>();
 
     const foundUser:UserData|undefined= isLegitUser.find((user)=> user.email === email);
     if(!foundUser){
-        return{isPasswordMatched:false,isEmailMatched:false}
+        return{isPasswordMatched:false,isEmailMatched:false,data:null}
     }
     if(foundUser.password === password ){
-        return  {isPasswordMatched:true ,isEmailMatched:true}
+        return  {isPasswordMatched:true ,isEmailMatched:true,data:foundUser}
     }
     return{
-        isPasswordMatched:false,isEmailMatched:true
+        isPasswordMatched:false,isEmailMatched:true,data:null
     }
 }

91e9e99e7e7463ff792ff8a3f0b19cc5f0cb3bbb
Sabareesh98421 <padmanabhansubramanian28@gmail.com>
2025-11-19 23:30:04 +0530

    Feat:added a new fcuntion that allow to save public file

--- serverUtils/fileHandling.ts
+++ serverUtils/fileHandling.ts
@@ -3,8 +3,10 @@ import path from "path";
 
 export class FilesHandling{
     private readonly  dataFilePath:string
+    private readonly publicFileName:string;
     constructor(fileName:string) {
         this.dataFilePath=path.join(process.cwd(), "data", fileName);
+        this.publicFileName=path.join(process.cwd(), "public");
     }
     async  ensureDataFile<T>(InitialData?:T) {
         try {
@@ -25,6 +27,16 @@ export class FilesHandling{
         const fileData = await fs.readFile(this.dataFilePath, "utf-8")
 
         return JSON.parse(fileData)
+    }
+
+    async savePublicFiles(file:File,fileName:string){
+        console.log(this.publicFileName);
+        const ext = path.extname(file.name);
+        const fileBuffer = Buffer.from(await file.arrayBuffer());
+        const filePath=path.join(this.publicFileName,`${fileName+ext}`);
+        await fs.writeFile(filePath, fileBuffer);
+        return `/${fileName}${ext}`;
+
     }
     
 }

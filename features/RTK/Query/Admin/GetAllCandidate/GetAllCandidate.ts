be287a49c6eee1d46658e623b73a82bebbd6d663
Sabareesh98421 <padmanabhansubramanian28@gmail.com>
2025-11-24 21:55:52 +0530

    update the getcandidate API to all user instead of only for admins

--- features/RTK/Query/Admin/GetAllCandidate/GetAllCandidate.ts
+++ features/RTK/Query/Admin/GetAllCandidate/GetAllCandidate.ts
@@ -1,26 +1,10 @@
 import {AppAPI} from "@/features/RTK/CreateAPI/DefineAppApi";
 import {Framework, ServerResponseWithData} from "@/sharedUtils/CustomTypes";
-import path from "path";
-
-
 const getAllCandidate = AppAPI.injectEndpoints({
     endpoints:(build)=>({
         allCandidates:build.query<ServerResponseWithData<Framework[]>,void>({
-            query:()=>"Admin/GetCandidates",
+            query:()=>"/GetCandidates",
         })
     })
 })
 export const {useAllCandidatesQuery} = getAllCandidate;
-
-
-
-
-function fetcher<T>(path:string,method:string,data?:T){
-    const headers={}
-
-    return fetch(path,{
-        method:method,
-        headers:headers,
-        body:data
-    })
-}

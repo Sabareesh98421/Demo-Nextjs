53ac87c63a1f414f861bf3fd7692c0eaac9723a8
Sabareesh98421 <padmanabhansubramanian28@gmail.com>
2025-11-25 17:49:40 +0530

    Fix:allowe structured votes count

--- app/api/vote/route.ts
+++ app/api/vote/route.ts
@@ -10,9 +10,9 @@ const fs = new FilesHandling("votes.json")
 export async function POST(req: Request) {
     const res = NextResponse;
     try {
-        await fs.ensureDataFile<VoteData[]>([]);
+        await fs.ensureDataFile<VoteData>({});
         const { email, frameWork } = await req.json();
-        console.log(req.json())
+        // console.log(req.json())
         if (!frameWork || !email) {
             return res.json({ message: "Missing FrameWork or email" }, { status: 400 })
         }
@@ -25,9 +25,10 @@ export async function POST(req: Request) {
         if (isAlreadyVotedUser) {
             return res.json({ message: "this user is already voted" }, { status: 403 })
         }
-        const targetFw = votes[trimmedFw];
+        let targetFw = votes[trimmedFw];
         if (!targetFw) {
-            return res.json({ message: "Invalid FrameWorkSelected" }, { status: 400 })
+            targetFw={emails:[],totalVotes:0};
+            votes[trimmedFw] = targetFw;
         }
 
         if (targetFw.emails.includes(loweredEmail)) {

46678a9c100ca541045b02f84f1751c78630b6be
Sabareesh98421 <padmanabhansubramanian28@gmail.com>
2025-12-01 11:27:12 +0530

    Fix:recursive Login had ben fixed

--- app/auth/google/callback/route.ts
+++ app/auth/google/callback/route.ts
@@ -1,9 +1,13 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getOAuthClient } from "@/serverUtils/googleOAuth";
-import { cookies } from "next/headers";
+import {NextRequest, NextResponse} from "next/server";
+import {getOAuthClient} from "@/serverUtils/googleOAuth";
+import {cookies} from "next/headers";
 import fs from "fs";
-import {type OAuth2Client} from "google-auth-library";
+import {LoginTicket, type OAuth2Client, TokenPayload} from "google-auth-library";
 import {ReadonlyRequestCookies} from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import {createJWT} from "@/serverUtils/JWT/jwt";
+import {Role} from "@/sharedUtils/CustomTypes";
+import {serverResponse} from "@/serverUtils/ServerResponse";
+
 
 export async function GET(req: NextRequest):Promise<NextResponse> {
     const code:string|null = req.nextUrl.searchParams.get("code");
@@ -15,15 +19,20 @@ export async function GET(req: NextRequest):Promise<NextResponse> {
     try {
         const oauth2Client:OAuth2Client = getOAuthClient();
         const { tokens } = await oauth2Client.getToken(code);
-
-
+        const ticket:LoginTicket = await oauth2Client.verifyIdToken({
+            idToken:tokens.id_token!,
+            audience:process.env.GOOGLE_CLIENT_ID,
+        })
+        const googleUser:TokenPayload|null= ticket.getPayload() ?? null;
         fs.writeFileSync("google_tokens.json", JSON.stringify(tokens, null, 2));
-
+        if(!googleUser || !googleUser.email){
+            return serverResponse({status:401,message:"unauthorized"});
+        }
 
         const cookieStore:ReadonlyRequestCookies = await  cookies();
         cookieStore.set({
             name: "token",
-            value: "google-authenticated",
+            value: createJWT(googleUser.email,Role.Admin),
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: "lax",
